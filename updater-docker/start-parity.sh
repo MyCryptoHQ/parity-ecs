227bbc5ebcb15e6425958a51a608ae639cdc98cd
Youngblood <lukey@8c859005c31c.ant.amazon.com>
2017-12-24 14:42:24 -0500

    Added update-docker build.

---
+++ updater-docker/start-parity.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+mkdir -p /root/.local
+aws s3 sync --delete --region $region s3://$bucket/$s3key/.local/ /root/.local/
+/parity/parity $* &
+sleep 1800
+pid=`ps -ef |grep parity/parity|grep -v grep|awk '{print $2}'`
+kill -HUP $pid
+sleep 30
+aws s3 sync --delete --region $region /root/.local/ s3://$bucket/$s3key/.local/
