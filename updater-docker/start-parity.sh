a072c781a5ede1f22589f23e3b8ada99c84a5ae0
Luke Youngblood <lukey@amazon.com>
2018-01-03 10:47:46 -0500

    Added error checking to S3 sync script to avoid potential corruption of stored blockchain in S3.

--- updater-docker/start-parity.sh
+++ updater-docker/start-parity.sh
@@ -2,6 +2,11 @@
 
 mkdir -p /root/.local
 aws s3 sync --delete --region $region s3://$bucket/$s3key/.local/ /root/.local/
+if [ $? -ne 0 ]
+then
+	echo "aws s3 sync command failed; exiting."
+	exit 1
+fi
 /parity/parity $* &
 sleep 1800
 pid=`ps -ef |grep parity/parity|grep -v grep|awk '{print $2}'`
