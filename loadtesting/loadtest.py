dbb03ca2cf2e9d9e152f0c4ff53f0c130c61fe2d
Luke Youngblood <lukey@amazon.com>
2018-01-04 17:23:31 -0500

    Removing loadtest.py

--- loadtesting/loadtest.py
+++
@@ -1,9 +0,0 @@
-import bees
-import json
-
-sOptions = '{"post_file":"data.json","contenttype":"application/json"}'
-options = json.loads(sOptions)
-
-bees.up(4,'beeswithmachineguns','us-east-1b')
-bees.attack('parity-service-20ccef1ff1cf7cbd.elb.us-east-1.amazonaws.com:8545',2,2,**options)
-bees.down()
